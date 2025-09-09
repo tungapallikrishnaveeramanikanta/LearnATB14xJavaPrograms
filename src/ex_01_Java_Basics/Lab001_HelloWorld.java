2cf9a2a1e1c650a32c3544051211dc65ffa04a57
manikanta <manikantatungapalli@gmail.com>
2025-09-09 16:36:32 +0530

    Test class 01

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -3,6 +3,7 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is the first intellij project");
 
     }
 }
