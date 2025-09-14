63975faf0252a7e6a7186e5a8106019912f642c1
manikanta <manikantatungapalli@gmail.com>
2025-09-14 17:34:13 +0530

    Table code change

--- src/ex_01_Java_Basics/Printing_Table.java
+++ src/ex_01_Java_Basics/Printing_Table.java
@@ -2,14 +2,16 @@ package ex_01_Java_Basics;
 import java.util.Scanner;
 
 
-public class Test_Class_02 {
+public class Printing_Table {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a table No : ");
         int table = scan.nextInt(); //9
         System.out.print("Enter a Limit: ");
         int n = scan.nextInt();  //12
-        for(int i=1;i<=n;i++) {
+        for(int i=1;i<=n;i++)
+        {
             System.out.println(table + " x " + i  + " = "  + (table * i));
         }
 
